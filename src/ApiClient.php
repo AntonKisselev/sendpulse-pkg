10627b601ac1eb5376fe765010f91a655ae46a23
AntonKisselev <antonkisselev@gmail.com>
2023-04-30 13:14:35 +0300

    проверка адреса в цикле пока не будет получен результат проверки

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -15,15 +15,30 @@ class ApiClient extends \Sendpulse\RestApi\ApiClient
         $data = array(
             'email' => $email,
         );
+        $tries  = 10;
 
-        $requestResult = $this->sendRequest('verifier-service/send-single-to-verify', 'POST', $data);
-        $requestResult = $this->sendRequest('verifier-service/get-single-result', 'GET', $data);
-        if ($requestResult->data->result == 1 && $requestResult->data->data
-            && $requestResult->data->data->checks && $requestResult->data->data->checks->status == 1){
-            return true;
-        }elseif ($requestResult->data->result == 1 && $requestResult->data->data
-            && $requestResult->data->data->checks && $requestResult->data->data->checks->status != 1){
-            return false;
+        while ($tries > 0) {
+            $tries--;
+            $requestResult = $this->sendRequest('verifier-service/get-single-result', 'GET', $data);
+            if (!$requestResult->data->result){
+                $this->sendRequest('verifier-service/send-single-to-verify', 'POST', $data);
+                $tries++;
+                continue;
+            }
+
+            ///if already checked and email valid
+            if ($requestResult->data->result == 1 && $requestResult->data->data
+                && $requestResult->data->data->checks && $requestResult->data->data->checks->status == 1) {
+                return true;
+            ///if not ckecked
+            } elseif ($requestResult->data->result == 1 && $requestResult->data->data
+                && $requestResult->data->data->checks && $requestResult->data->data->checks->status == 0) {
+                sleep(1);
+                continue;
+            /// already checked and email not valid
+            }else{
+                return false;
+            }
         }
         throw new \Exception('error getting data from sendpulse');
     }
